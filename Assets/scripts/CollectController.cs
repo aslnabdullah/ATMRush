c2a00b73e7b64a0625ed6c9120513705ff86acae
aslnabdullah <abdullahaslan2510@gmail.com>
2022-06-06 22:05:47 +0300

    Initial commit

---
+++ Assets/scripts/CollectController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("collect"))
+        {
+            other.gameObject.transform.position = transform.position + Vector3.forward;
+            Destroy(gameObject.GetComponent<CollectController>());
+            other.gameObject.AddComponent<CollectController>();
+            other.gameObject.GetComponent<BoxCollider>().isTrigger = false;
+            other.gameObject.AddComponent<NodeMovement>();
+            other.gameObject.GetComponent<NodeMovement>().connectedNode = transform;
+            other.gameObject.tag = "collected";
+
+        }
+    }
+
+}
---
+++ Assets/scripts/movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed;
+    [SerializeField] float horizontalSpeed;
+    float hor;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        hor = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(hor * horizontalSpeed * Time.deltaTime, 0, movementSpeed * Time.deltaTime));    
+        
+    }
+}
